02334617e47305fe82fa7c754b8e8e1b8caecdc0
SoMuchForSubtlety <s0muchfrsubtlety@gmail.com>
2019-06-27 15:53:37 +0200

    make comments more readable

--- opendj.go
+++ opendj.go
@@ -25,18 +25,19 @@ type handlers struct {
 	errorHander    func(error)
 }
 
-// Media represents a video or song that can be streamed
-// this can be anything youtube-dl supports
+// Media represents a video or song that can be streamed.
+//
+// this can be anything youtube-dl supports.
 type Media struct {
 	Title    string
 	URL      string
 	Duration time.Duration
 }
 
-// A QueueEntry represents media and metadata the can be ented into a queue
+// A QueueEntry represents media and metadata the can be ented into a queue.
 type QueueEntry struct {
 	Media      Media
-	User       string
+	Owner      string
 	Dedication string
 }
 
@@ -45,9 +46,7 @@ type queue struct {
 	sync.Mutex
 }
 
-const youtubeURLStart = "https://www.youtube.com/watch?v="
-
-// NewDj returns a new Dj struct
+// NewDj initializes and returns a new Dj struct.
 func NewDj(queue []QueueEntry) (dj *Dj, err error) {
 	dj = &Dj{}
 	dj.waitingQueue.Items = queue
@@ -55,33 +54,35 @@ func NewDj(queue []QueueEntry) (dj *Dj, err error) {
 	return dj, nil
 }
 
-// AddNewSongHandler adds a function that will be called every time a new song starts playing
+// AddNewSongHandler adds a function that will be called every time a new song starts playing.
 func (dj *Dj) AddNewSongHandler(f func(QueueEntry)) {
 	dj.handlers.newSongHandler = f
 }
 
-// AddPlaybackErrorHandler adds a function that will be called every time an error occurs during playback
-// in effect this mean it will be called every time ffmpeg or youtube-dl exit with an error
-// sometimes ffmpeg can exit with code 1 even though the song was streamed successfully
+// AddPlaybackErrorHandler adds a function that will be called every time an error occurs during playback.
+//
+// In effect this mean it will be called every time ffmpeg or youtube-dl exit with an error.
+// Sometimes ffmpeg can exit with code 1 even though the song was streamed successfully.
 func (dj *Dj) AddPlaybackErrorHandler(f func(error)) {
 	dj.handlers.errorHander = f
 }
 
-// Queue return the current queue as a list of queue entries
+// Queue return the current queue as a list of queue entries.
 func (dj *Dj) Queue() []QueueEntry {
 	return dj.waitingQueue.Items
 }
 
-// AddEntry adds the passed QueueEntry at the end of the queue
+// AddEntry adds the passed QueueEntry at the end of the queue.
 func (dj *Dj) AddEntry(newEntry QueueEntry) {
 	dj.waitingQueue.Lock()
 	dj.waitingQueue.Items = append(dj.waitingQueue.Items, newEntry)
 	dj.waitingQueue.Unlock()
 }
 
-// InsertEntry inserts the passed QueueEntry into the queue at the given index
-// if the index is too high it has the same effect as AddEntry()
-// returns an error if the index is < 0
+// InsertEntry inserts the passed QueueEntry into the queue at the given index.
+//
+// if the index is too high it has the same effect as AddEntry().
+// returns an error if the index is < 0.
 func (dj *Dj) InsertEntry(newEntry QueueEntry, index int) error {
 	dj.waitingQueue.Lock()
 	defer dj.waitingQueue.Unlock()
@@ -99,7 +100,8 @@ func (dj *Dj) InsertEntry(newEntry QueueEntry, index int) error {
 }
 
 // RemoveIndex removes the element the given index from the queue
-// returns an error if the index is out of range
+//
+// returns an error if the index is out of range.
 func (dj *Dj) RemoveIndex(index int) error {
 	dj.waitingQueue.Lock()
 	defer dj.waitingQueue.Unlock()
@@ -110,7 +112,8 @@ func (dj *Dj) RemoveIndex(index int) error {
 	return nil
 }
 
-// ChangeIndex swaps the element the given index for the provided one
+// ChangeIndex swaps the QueueEntry the index for the provided one
+//
 // returns an error if the index is out of range
 func (dj *Dj) ChangeIndex(newEntry QueueEntry, index int) error {
 	dj.waitingQueue.Lock()
@@ -152,9 +155,10 @@ func (dj *Dj) EntryAtIndex(index int) (QueueEntry, error) {
 	return entry, nil
 }
 
-// Play starts the playback to the given RTMP server
-// if nothing is in the playlist it waits for new content to be added
-// any erncountered errors are sent to the playbackErrorFeed
+// Play starts the playback to the given RTMP server.
+//
+// If nothing is in the playlist it waits for new content to be added.
+// Any encoutered errors are handled by the errorHandler.
 func (dj *Dj) Play(rtmpServer string) {
 	for {
 		entry, err := dj.pop()
@@ -202,44 +206,30 @@ func (dj *Dj) Play(rtmpServer string) {
 	}
 }
 
-// UserPosition returns a slice of all the position in the queue that belong to the given user
+// UserPosition returns a slice of all the position in the queue that belong to the given user.
 func (dj *Dj) UserPosition(nick string) (positions []int) {
 	dj.waitingQueue.Lock()
 	defer dj.waitingQueue.Unlock()
 
 	for i, content := range dj.waitingQueue.Items {
-		if content.User == nick {
+		if content.Owner == nick {
 			positions = append(positions, i)
 		}
 	}
 	return positions
 }
 
-// DurationUntilUser returns a slice of all the durations to the songs in the queue that belong to the given user
+// DurationUntilUser returns a slice of all the durations to the songs in the queue that belong to the given user.
 func (dj *Dj) DurationUntilUser(nick string) (durations []time.Duration) {
 	dj.waitingQueue.Lock()
 	defer dj.waitingQueue.Unlock()
 
 	dur := dj.currentEntry.Media.Duration - time.Since(dj.songStarted)
 	for _, content := range dj.waitingQueue.Items {
-		if content.User == nick {
+		if content.Owner == nick {
 			durations = append(durations, dur)
 		}
 		dur += content.Media.Duration
 	}
 	return durations
-}
-
-// ChangeDedication changes the dedication of the queue entry at the given index
-// returns an error if the index is out of range
-func (dj *Dj) ChangeDedication(index int, dedication string) error {
-	dj.waitingQueue.Lock()
-	defer dj.waitingQueue.Unlock()
-
-	if index < 0 || index >= len(dj.waitingQueue.Items) {
-		return errors.New("index out of range")
-	}
-
-	dj.waitingQueue.Items[index].Dedication = dedication
-	return nil
 }
